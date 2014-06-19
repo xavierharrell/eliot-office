a30974015a960abd1d7ae82b691fbcc5d4e9574c
Xavier Harrell <xavier@e-mg.com>
2014-06-19 15:53:09 -0500

    Added waypoints. Moved testimonial section to its own.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
+/* Scroller for the Testimonials */
 $(function () {
     $('.testi-frame').slick({
             dots: true,
+            autoplay: true
     });
         $('.video-frame').slick({
             dots: true,
             slidesToShow: 1,
             slidesToScroll: 1,
-            autoplay: true
+            autoplay: false
     });
 });
+/* Waypoint and Animation */
+$('.animate').css('opacity', '0');
+$('.animate').waypoint(function(){
+    var animation = $(this).attr('data-animate'),
+        $this = $(this);
+
+        $this.addClass(animation);
+        $this.css('opacity', '1');
+        $this.addClass('animated');
+    },{offset: '50%'});
+
 
 $(document).foundation();
 
